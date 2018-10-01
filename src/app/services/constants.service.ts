a0c1923b61fdf03b1f9a5cf5d3d0eeb0580e0734
Omar Perozo <operozo@intelenz.com>
2018-10-01 13:18:07 -0400

    finished app

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -14,14 +14,4 @@ export class ConstantsService {
     { name: 'name', label: 'Nombre', filter: true, sortable: true },
     { name: 'description', label: 'Descripción', filter: true, sortable: false },
   ];
-
-  public availableCountries = [
-    { id: 1, value: 'Venezuela' },
-    { id: 2, value: 'Iceland' },
-    { id: 3, value: 'Japan' },
-    { id: 4, value: 'France' },
-    { id: 5, value: 'Italy' },
-    { id: 6, value: 'United States' }
-  ];
-
 }
--- src/app/user/userdata/userdata.component.ts
+++ src/app/user/userdata/userdata.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, ITdDataTableColumn } from '@covalent/core/data-table';
-import { ConstantsService, UserService } from './../../services';
 import { TdLoadingService, LoadingType, LoadingMode } from '@covalent/core/loading';
+import { ConstantsService, UserService, UtilService } from './../../services';
 import { IUser } from '../../models/beans/user';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-userdata',
@@ -24,12 +25,15 @@ export class UserDataComponent implements OnInit {
   public sortBy = 'name';
   public selectedRows: any[] = [];
   public sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+  public userForm: any;
 
   constructor(private _dataTableService: TdDataTableService, private constantsService: ConstantsService,
-    private userService: UserService, private loadingService: TdLoadingService) { }
+    private userService: UserService, private loadingService: TdLoadingService,
+    private utilService: UtilService, private formBuilder: FormBuilder) {
+
+  }
 
   ngOnInit(): void {
-    this.countries = this.constantsService.availableCountries;
     this.loadingService.create({
       color: 'accent',
       name: 'service',
@@ -39,8 +43,16 @@ export class UserDataComponent implements OnInit {
     this.loadingService.register('service');
     this.hasEntered = true;
     this.columns = this.constantsService.userTableColumns;
+    this.initializeForms();
     this.getUsers();
     this.filter();
+  }
+
+  initializeForms() {
+    this.userForm = this.formBuilder.group({
+      country: [this.user.country, Validators.compose([Validators.required, Validators.minLength(1)])],
+      description: [this.user.description]
+    });
   }
 
   getUsers() {
@@ -49,7 +61,6 @@ export class UserDataComponent implements OnInit {
       this.filteredData = this.data;
       this.loadingService.resolve('service');
     });
-
   }
 
   sort(sortEvent: ITdDataTableSortChangeEvent): void {
@@ -61,12 +72,6 @@ export class UserDataComponent implements OnInit {
   search(searchTerm: string): void {
     this.searchTerm = searchTerm;
     this.filter();
-  }
-
-
-  showAlert(event: any): void {
-    const row: any = event.row;
-    // .. do something with event.row
   }
 
   filter(): void {
@@ -83,4 +88,7 @@ export class UserDataComponent implements OnInit {
     this.filteredData = newData;
   }
 
+  save() {
+    this.utilService.openSnackBar('Cambios efectuados correctamente!');
+  }
 }
